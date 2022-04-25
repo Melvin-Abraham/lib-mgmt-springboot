ae4586386c47e3931a1b9eea8b677b24c50df23a
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-04-25 16:17:16 +0530

    test: Add library test for retrieving all books
    Add test for `library.getBooks()` functionality

--- src/test/java/com/example/library/LibraryTest.java
+++ src/test/java/com/example/library/LibraryTest.java
@@ -2,6 +2,8 @@ package com.example.library;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LibraryTest {
@@ -14,5 +16,19 @@ public class LibraryTest {
 
         Book retrievedBook = library.getBookById(1);
         assertEquals(retrievedBook, newBook);
+    }
+
+    @Test
+    void shouldBeAbleToReturnAllBooks() {
+        Library library = new Library();
+        Book firstBook = new Book(1, "Book 1", "Author 1");
+        Book secondBook = new Book(2, "Book 2", "Author 2");
+
+        library.addBook(firstBook);
+        library.addBook(secondBook);
+        List<Book> allBooks = library.getBooks();
+
+        // Check if the length of books returned match the number of books added
+        assertEquals(2, allBooks.size());
     }
 }
